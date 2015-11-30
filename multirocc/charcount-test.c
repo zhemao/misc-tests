cd0f52d80ae371deb04cfcec575f9c7b180964fd
Howard Mao <zhehao.mao@gmail.com>
2015-11-29 19:05:51 -0800

    make charcount test more unique

--- multirocc/charcount-test.c
+++ multirocc/charcount-test.c
@@ -4,8 +4,8 @@ char string[64] = "The quick brown fox jumped over the lazy dog";
 
 int main(void)
 {
-	unsigned long count = count_chars(string, 'o');
-	if (count != 4)
+	unsigned long count = count_chars(string + 14, 'o');
+	if (count != 3)
 		return count + 1;
 	return 0;
 }
